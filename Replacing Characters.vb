670104aa09bb0527f3e3e3752b26ca33ee832f65
salmanhassan03 <55985636+salmanhassan03@users.noreply.github.com>
2019-10-03 03:40:21 -0700

    This is the source code for Replacing characters

---
+++ Replacing Characters.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim STR1, STR2, Char1, Char2, NextChar As String
+        Dim Count As Integer
+
+        STR1 = ""
+        STR2 = ""
+        Char1 = ""
+        Char2 = ""
+        NextChar = ""
+        Count = 0
+
+        Console.Write("Please Enter Your String: ")
+        STR1 = Console.ReadLine
+        Console.Write("Please Enter The Character You Want To Remove: ")
+        Char1 = Console.ReadLine
+        Console.Write("Please Enter The Character You Want To Replace With: ")
+        Char2 = Console.ReadLine
+
+        For Count = 1 To Len(STR1)
+            NextChar = Mid(STR1, Count, 1)
+            If NextChar = Char1 Then
+                NextChar = Char2
+            End If
+            STR2 = STR2 & NextChar
+        Next
+        Console.Write("Your String Output is: " & STR2)
+
+        Console.ReadKey()
+
+    End Sub
+End Module
