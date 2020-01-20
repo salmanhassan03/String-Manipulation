d46cb963f503891919b14fa081eaefaa114abc3c
salmanhassan03 <55985636+salmanhassan03@users.noreply.github.com>
2020-01-20 17:10:54 +0500

    Add files via upload

---
+++ Membership data.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim PlayerName As String
+        Dim MemberID As String
+        Dim More As String
+
+        PlayerName = ""
+        MemberID = ""
+
+
+        FileOpen(1, "D:Membership Data.txt", OpenMode.Input)
+        Do
+            Console.Write("Enter player name: ") : PlayerName = Console.ReadLine
+            Console.Write("Enter member ID: ") : MemberID = Console.ReadLine
+
+            Input(1, PlayerName)
+            Input(1, MemberID)
+
+            Console.WriteLine(" Do you want to add another record? (Y/N) ")
+            More = Console.ReadLine
+
+        Loop Until UCase(More) = "N"
+        FileClose(1)
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
